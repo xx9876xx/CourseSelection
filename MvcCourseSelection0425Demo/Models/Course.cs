94db860a391bd256f409bc92b6361e0017448eb9
Allen_Chang_tpPC <allen.chang@thinkpower-info.com>
2018-04-26 10:01:21 +0800

    1.在Course類別加入[Display(Name="XXX")]，使表格head可以顯示中文項目名稱。 2.在Selection的View的表格加入課程上限的欄位。

--- MvcCourseSelection0425Demo/Models/Course.cs
+++ MvcCourseSelection0425Demo/Models/Course.cs
@@ -2,19 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcCourseSelection0425Demo.Models
 {
     public class Course
     {
+        [Display(Name = "課程代碼")]
         public int id { get; set; }
+        [Display(Name = "課程名稱")]
         public string courseName { get; set; }
+        [Display(Name = "開課學院")]
         public string college { get; set; }
+        [Display(Name = "開課系所")]
         public string department { get; set; }
+        [Display(Name = "項次")]
         public int term { get; set; }
+        [Display(Name = "星期")]
         public int day { get; set; }
+        [Display(Name = "時段")]
         public string period { get; set; }
+        [Display(Name = "老師")]
         public string teacher { get; set; }
+        [Display(Name = "人數上限")]
         public int limit_up { get; set; }
 
     }
